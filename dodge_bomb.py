035860aa76125c72f1e590ae1c538906ed8103b0
c0a22127 <teu_github@6mile.dev>
2023-10-03 16:32:45 +0900

    演習課題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import sys
 import pygame as pg
 from random import randint
+import time
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -15,7 +16,6 @@ move_lst = {
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, 5),
 }
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -25,7 +25,8 @@ def main():
     # こうかとん作成
     # ===========================
     kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_fliped_1 = pg.transform.flip(kk_img, True, False)
+
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900, 400)  # 画像の中心座標を設定
 
@@ -42,6 +43,8 @@ def main():
 
     clock = pg.time.Clock()
     tmr = 0
+
+    # 爆弾の移動量
     vx, vy = 5, 5
 
     while True:
@@ -51,6 +54,13 @@ def main():
             
         if kk_rct.colliderect(bomb_rct):
             print("GAME OVER")
+            kk_pien = pg.image.load("fig/8.png")
+            kk_pien = pg.transform.rotozoom(kk_pien, 0, 2.0)
+            pien_rct = kk_pien.get_rect()
+            pien_rct = kk_rct
+            screen.blit(kk_pien, pien_rct)
+            pg.display.update()
+            time.sleep(3)
             return
         
         key_lst = pg.key.get_pressed()
@@ -74,13 +84,13 @@ def main():
             vx *= -1
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(pg.transform.rotozoom(kk_img, 0, 2.0), kk_rct)
         screen.blit(circle, bomb_rct)
-        pg.display.update()
+        pg.display.flip()
         tmr += 1
         clock.tick(50)
 
-def is_obj_outside(obj: tuple):
+def is_obj_outside(obj: tuple) -> tuple:
     """
     オブジェクトの画面外判定 (obj: tuple) -> tuple(bool, bool))\n
     引数: obj: こうかとんRect, 爆弾Rect\n
